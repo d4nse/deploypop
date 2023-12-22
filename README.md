b8846e6c3a9d5a9de02bb652d98fc3cf8e35317b
Valde Vi <danse@pop-os.localdomain>
2023-12-22 16:03:17 +0500

    Restructured the script, added dconf files, added apt lists

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Deploy POP!
 
+## TODO
+- Add some checks before installation to make sure nothing is overwritten
+- Add an option to re-install (overwrite) something if needed
+
+## TEMP NOTES
+some packages to keep in mind:
+### APT
+ttf-mscorefonts-installer
+### FLATPAK
+com.obsproject.Studio
+md.obsidian.Obsidian
+org.kde.kdenlive
+org.telegram.desktop
+
+
 ## Notes
 Some notes on how to do automation in Gnome
 
@@ -13,3 +28,11 @@ Load (import) terminal profiles using dconf:
 dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
 ```
 if you get `error: Key file does not start with a group` replace/add leading line of your dump with `[/]`
+
+### vscode 
+#### clangd
+g++-12 package on ubuntu derivatives fixes the inability of clangd to find basic headers
+#### latex
+in addition to texlive install these for format to work:
+libyaml-tiny-perl
+libfile-homedir-perl
--- deploy.sh
+++ deploy.sh
@@ -1,72 +1,146 @@
 #!/usr/bin/env bash
 
-# Some packages to keep in mind:
-# ttf-mscorefonts-installer
-# ffmpeg vlc mpv feh
-# lm-sensors tree neofetch
-# keepassxc
-
-SCRIPT_NAME="rice"
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-RICE="$SCRIPT_DIR/rice"
+RICE_DIR="$SCRIPT_DIR/rice"
 
-echo() {
-    command echo "$SCRIPT_NAME: $1"
-}
-
-make_dirs() {
-    if [[ ! -d "$HOME/.fonts" ]]; then
-        mkdir "$HOME/.fonts"
-    fi
-    if [[ ! -d "$HOME/opt" ]]; then
-        mkdir "$HOME/opt"
-    fi
-}
-
-install_meslo_fonts() {
-    # requirements: curl
-    echo "Downloading Meslo fonts..."
-    curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output "$HOME/.fonts/MesloLGS NF Regular.ttf"
-    curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output "$HOME/.fonts/MesloLGS NF Bold.ttf"
-    curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output "$HOME/.fonts/MesloLGS NF Italic.ttf"
-    curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf --output "$HOME/.fonts/MesloLGS Bold Italic.ttf"
-    fc-cache -f
-    echo "Meslo fonts successfully installed to ~/.fonts"
-}
-
-install_zsh() {
-    # requirements: zsh zsh-syntax-highlighting git
-    echo "Downloading powerlevel10k..."
-    git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/opt/powerlevel10k"
-    echo "Dropping dots to home..."
-    cp {"$RICE/dots/.zshrc","$RICE/dots/.p10k"} "$HOME/"
-    echo "CHSHing zsh..."
-    chsh -s "$(which zsh)" "$(whoami)"
-    echo "Successfully installed ZSH and rice, log in and out for changes to take effect"
+print_help() {
+    echo "Script for automatic system deployment POP!_OS edition."
+    echo
+    echo "Usage: $0 [a|m|h]"
+    echo
+    echo "  -a, --automatic     install everything without prompting"
+    echo "  -m, --manual        enter manual mode and choose what to install"
+    echo "  -h, --help          print this page"
 }
 
 install_gogh_theme() {
     # requirements: dconf-cli uuid-runtime
-    theme_name="synthwave"
-    gogh_root="$HOME/opt/gogh-themes"
-    theme_root="$gogh_root/installs"
+    GOGH_THEME_NAME="synthwave"
+    GOGH_INSTALL_PATH="$HOME/opt/gogh-themes"
+    GOGH_THEME_PATH="$GOGH_INSTALL_PATH/installs/$GOGH_THEME_NAME.sh"
 
-    if [[ ! -d "$gogh_root" ]]; then
-        echo "Downloading Gogh themes into ~/opt/gogh-themes..."
-        git clone --quiet https://github.com/Gogh-Co/Gogh.git "$gogh_root"
+    # Install Gogh
+    if [[ -d "$GOGH_INSTALL_PATH" ]]; then
+        echo "Gogh is already installed to $GOGH_INSTALL_PATH"
     else
-        echo "Gogh themes are already installed in ~/opt/gogh-themes"
+        echo "Downloading latest Gogh..."
+        git clone --quiet https://github.com/Gogh-Co/Gogh.git "$GOGH_INSTALL_PATH"
+        echo "Successfully installed Gogh to '$GOGH_INSTALL_PATH'"
     fi
 
-    if [[ -f "$theme_root/$theme_name.sh" ]]; then
-        export TERMINAL=gnome-terminal
-        bash "$theme_root/$theme_name.sh"
-        echo "Gogh theme \"$theme_name\" successfully installed"
-        echo "Change terminal profile to apply theme"
-        #last_installed_theme_id=$(gsettings get org.gnome.Terminal.ProfilesList list | sed "s/'/\"/g" | jq '.[-1]')
+    # Install theme
+    if [[ -f "$GOGH_THEME_PATH" ]]; then
+        if [[ -f "$GOGH_THEME_PATH.installed" ]]; then
+            echo "Terminal theme '$GOGH_THEME_NAME' is already installed"
+        else
+            export TERMINAL=gnome-terminal
+            bash "$GOGH_THEME_PATH"
+            touch "$GOGH_THEME_PATH.installed"
+            echo "Terminal theme '$GOGH_THEME_NAME' successfully installed"
+        fi
     else
-        echo "Gogh theme \"$theme_name\" was not found in ~/opt/gogh-themes"
+        echo "Terminal theme '$GOGH_THEME_NAME' was not found in '$GOGH_INSTALL_PATH/installs/'"
     fi
+}
+
+install_neovim() {
+    NVIM_EXTRACT_PATH="/tmp/nvim-linux64"
+    NVIM_INSTALL_PATH="$HOME/opt/nvim"
+    NVIM_DOT_PATH="$HOME/.config/nvim"
+
+    # Check path exists
+    if [[ ! -d "$NVIM_DOT_PATH" ]]; then
+        mkdir -p "$NVIM_DOT_PATH"
+    fi
+
+    # Install neovim
+    if [[ -d "$NVIM_INSTALL_PATH" ]]; then
+        echo "Neovim is already installed to $NVIM_INSTALL_PATH"
+    else
+        echo "Downloading latest Neovim..."
+        curl --silent --location https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz --output "$NVIM_EXTRACT_PATH.tar.gz" &&
+            tar --extract --file="$NVIM_EXTRACT_PATH.tar.gz" --directory="/tmp/" &&
+            mv "$NVIM_EXTRACT_PATH" "$NVIM_INSTALL_PATH"
+        echo "Successfully installed Neovim to '$NVIM_INSTALL_PATH'"
+    fi
+
+    echo "Dropping dots to $NVIM_DOT_PATH..."
+    cp "$RICE_DIR/dots/init.vim" "$NVIM_DOT_PATH"
+}
+
+install_fonts() {
+    FONT_PATH="$HOME/.fonts"
+    MESLO_REGULAR="$FONT_PATH/MesloLGS_NF_Regular.ttf"
+    MESLO_BOLD="$FONT_PATH/MesloLGS_NF_Bold.ttf"
+    MESLO_ITALIC="$FONT_PATH/MesloLGS_NF_Italic.ttf"
+    MESLO_BOLD_ITALIC="$FONT_PATH/MesloLGS_NF_Bold_Italic.ttf"
+
+    # Check path exists
+    if [[ ! -d "$FONT_PATH" ]]; then
+        mkdir "$FONT_PATH"
+    fi
+
+    # Check if font is installed, download if not
+    if [[ ! -f "$MESLO_REGULAR" ]]; then
+        echo "Downloading Meslo Regular..."
+        curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output "$MESLO_REGULAR"
+    fi
+    if [[ ! -f "$MESLO_BOLD" ]]; then
+        echo "Downloading Meslo Bold..."
+        curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output "$MESLO_BOLD"
+    fi
+    if [[ ! -f "$MESLO_ITALIC" ]]; then
+        echo "Downloading Meslo Italic..."
+        curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output "$MESLO_ITALIC"
+    fi
+    if [[ ! -f "$MESLO_BOLD_ITALIC" ]]; then
+        echo "Downloading Meslo Bold Italic..."
+        curl --silent --location https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf --output "$MESLO_BOLD_ITALIC"
+    fi
+    echo "Rebuilding font cache..."
+    fc-cache -f
+}
+
+install_zsh_config() {
+    # requirements: zsh zsh-syntax-highlighting
+    POWER_INSTALL_PATH="$HOME/opt/powerlevel10k"
+
+    # Install powerlevel10k theme
+    if [[ -d "$POWER_INSTALL_PATH" ]]; then
+        echo "Powerlevel10k is already installed to $POWER_INSTALL_PATH"
+    else
+        echo "Downloading powerlevel10k..."
+        git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWER_INSTALL_PATH"
+    fi
+
+    # Copy dotfiles
+    echo "Dropping dots to home..."
+    cp {"$RICE_DIR/dots/.zshrc","$RICE_DIR/dots/.p10k"} "$HOME/"
+
+    # Change shell to zsh
+    if [ "$SHELL" != "/usr/bin/zsh" ]; then
+        echo "Changing shell to ZSH..."
+        chsh -s "$(which zsh)" "$(whoami)"
+    else
+        echo "Shell is already set to ZSH"
+    fi
+
+    echo "Successfully installed ZSH configuration"
+}
+
+add_repo_librewolf() {
+    # Installation script is taken from https://librewolf.net/installation/debian
+    # requirements: wget gnupg lsb-release apt-transport-https ca-certificates
+    distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
+    wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+    sudo tee /etc/apt/sources.list.d/librewolf.sources <<EOF >/dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: $distro
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
 }
 
 install_vscode() {
@@ -99,33 +173,6 @@ install_vscode() {
     return 0
 }
 
-install_neovim() {
-    nvim_path="$HOME/opt/nvim-linux64"
-    echo "Downloading latest Neovim..."
-    curl --silent --location https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz --output "$nvim_path.tar.gz" &&
-        tar --extract --file="$nvim_path.tar.gz" --directory="$HOME/opt/" &&
-        rm "$nvim_path.tar.gz" &&
-        mv "$nvim_path" "$HOME/opt/nvim/"
-    echo "Successfully installed Neovim to: '~/opt/nvim/'"
-}
-
-install_librewolf() {
-    # Installation script is taken from https://librewolf.net/installation/debian
-    sudo apt update && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
-    distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
-    wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-    sudo tee /etc/apt/sources.list.d/librewolf.sources <<EOF >/dev/null
-Types: deb
-URIs: https://deb.librewolf.net
-Suites: $distro
-Components: main
-Architectures: amd64
-Signed-By: /usr/share/keyrings/librewolf.gpg
-EOF
-    sudo apt update
-    sudo apt install librewolf -y
-}
-
 install_reaper() {
     # ADD REAPER CONFIGURATION ALONG WITH YABRIDGE N SHIT
     # qjackctl will usually pull jackd and all the dependencies they need together
@@ -133,9 +180,41 @@ install_reaper() {
     return 0
 }
 
-update_neovim() {
-    nvim_path="$HOME/opt/nvim"
-    echo "Updating Neovim..."
-    rm -rf "$nvim_path"
+automatic_wizard() {
+    # Get requirements
+    sudo apt-get update 1>/dev/null &&
+        xargs sudo apt-get install -y <"$SCRIPT_DIR/requirements.txt" 1>/dev/null
+
+    # Setup directories
+    if [[ ! -d "$HOME/opt" ]]; then
+        mkdir "$HOME/opt"
+    fi
+
+    # Run installs
+    install_fonts
+    install_gogh_theme
     install_neovim
+    install_zsh_config
+
+    # Get the rest of the apps
+    sudo apt-get update 1>/dev/null &&
+        xargs sudo apt-get install -y <"$SCRIPT_DIR/apps.txt" 1>/dev/null
+
+    echo "Done. Log out for changes to take effect."
 }
+
+getopts amh opt
+case $opt in
+a)
+    echo "Automatic installation is chosen"
+    ;;
+m)
+    echo "Manual installation is chosen"
+    ;;
+h)
+    print_help
+    ;;
+*)
+    print_help
+    ;;
+esac
